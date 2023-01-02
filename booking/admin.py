238a5ffde53b45c2ee1121a360b193078718ff07
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-02 14:25:21 +0000

    changed table.model, deleted DB and migrated again

--- booking/admin.py
+++ booking/admin.py
@@ -5,7 +5,6 @@ from .models import Review, Table
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 
-    prepopulated_fields = {'slug': ('title', )}
     list_filter = ('created_on',)
     list_display = ('title', 'slug', 'content', 'stars', 'created_on', 'status')
     search_fields = ['title', 'content']
@@ -18,7 +17,6 @@ class ReviewAdmin(admin.ModelAdmin):
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
 
-    prepopulated_fields = {'name': ('table_number', )}
     list_filter = ('created_on',)
     list_display = ('table_number', 'name', 'created_on',)
     search_fields = ['table_number', ]
--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,26 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, "Awaiting"), (1, "Approved"))
+
+BOOKING_TIMES = (
+    ('8:00', '8:00'),
+    ('9:00', '9:00'),
+    ('10:00', '10:00'),
+    ('11:00', '11:00'),
+    ('12:00', '12:00'),
+    ('13:00', '13:00'),
+    ('14:00', '14:00'),
+    ('15:00', '15:00'),
+    ('16:00', '16:00'),
+)
+
+TABLE_SEATS = (
+    ('2', '2'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+)
 
 
 class Review(models.Model):
@@ -35,12 +54,14 @@ class Review(models.Model):
 
 class Table(models.Model):
     table_number = models.IntegerField(unique=True)
-    name = models.CharField(max_length=200, unique=True, default='table')
-    num_seats = models.IntegerField()
+    name = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
+    booked = models.BooleanField(default=False)
+    num_seats = models.CharField(max_length=10, choices=TABLE_SEATS, default='2')
+    time = models.CharField(max_length=10, choices=BOOKING_TIMES, default='8:00')
 
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
-        return self.name
+        return f"Table number {self.table_number}"
