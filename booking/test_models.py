72f80267e5760a35fd02cd9c6a72c36ca4c414ba
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-10 09:56:58 +0000

    add test for delete, changed create table test, both pass

--- booking/test_models.py
+++ booking/test_models.py
@@ -4,6 +4,15 @@ from .models import Table
 
 class TestModels(TestCase):
 
-    def test_done_defaults_to_false(self):
+    def test_create_table(self):
+        """Create a table and test of the DB has 1 entry"""
         table = Table.objects.create(table_number=1, num_seats=2)
-        self.assertFalse(table.has_booking)
+        tables = Table.objects.all()
+        self.assertEqual(len(tables), 1)
+
+    def test_delete_table(self):
+        """Delete a table and test of the DB has 0 entries"""
+        table = Table.objects.create(table_number=1, num_seats=2)
+        tables = Table.objects.all()
+        table.delete()
+        self.assertEqual(len(tables), 0)
