6bb414c46a4432ff81ffe467742dc7e2f508dbff
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-10 10:53:25 +0000

    add test for bookings, create and delele, add test for user creation

--- booking/test_models.py
+++ booking/test_models.py
@@ -1,18 +1,47 @@
 from django.test import TestCase
-from .models import Table
+from .models import Table, Booking, User
+import datetime
 
 
 class TestModels(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.test_user = User.objects.create(username='test')
+
+    def test_create_user(self):
+        """Create a user and test if the user exists"""
+        # test_user = User.objects.create(username='test')
+        self.assertTrue(self.test_user)
+
     def test_create_table(self):
-        """Create a table and test of the DB has 1 entry"""
+        """Create a table and test if the DB has 1 entry"""
         table = Table.objects.create(table_number=1, num_seats=2)
         tables = Table.objects.all()
         self.assertEqual(len(tables), 1)
 
     def test_delete_table(self):
-        """Delete a table and test of the DB has 0 entries"""
+        """Delete a table and test if the DB has 0 entries"""
         table = Table.objects.create(table_number=1, num_seats=2)
         tables = Table.objects.all()
         table.delete()
         self.assertEqual(len(tables), 0)
+
+    def test_create_booking(self):
+        """Create a booking and test if the DB has 1 entry"""
+        table = Table.objects.create(table_number=1, num_seats=2)
+        booking = Booking.objects.create(table=table, booked_by=self.test_user, booking_time=datetime.time(10, 00, 00))
+        bookings = Booking.objects.all()
+        self.assertEqual(len(bookings), 1)
+
+    def test_delete_booking(self):
+        """Delete a table and test if the DB has 0 entries"""
+        table = Table.objects.create(table_number=1, num_seats=2)
+        booking = Booking.objects.create(table=table, booked_by=self.test_user, booking_time=datetime.time(10, 00, 00))
+        bookings = Booking.objects.all()
+        booking.delete()
+        self.assertEqual(len(bookings), 0)
+
+    @classmethod
+    def tearDownClass(cls):
+        ...
