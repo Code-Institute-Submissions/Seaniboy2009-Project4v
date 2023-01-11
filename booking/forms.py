4d7531a72f8b4915db627c8b4979c3433624a5ac
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-11 15:23:57 +0000

    chenaged Css for main image, txt colour

--- booking/forms.py
+++ booking/forms.py
@@ -3,11 +3,19 @@ from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django import forms
 import datetime
 
+NUMBER_OF_SEATS = [
+    ('2', '2'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+]
+
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         booking_time = forms.ChoiceField(choices=TIME_SLOTS,)
+        number_of_guests = forms.ChoiceField(choices=NUMBER_OF_SEATS,)
         fields = ('number_of_guests', 'booking_date', 'booking_time',)
         widgets = {
             'booking_date': forms.TextInput(attrs={'type': 'date'}),
