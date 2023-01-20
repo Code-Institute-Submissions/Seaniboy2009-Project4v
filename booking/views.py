2bcaff838c8fe06cff1873001ebccfb83ed67f87
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-20 14:35:33 +0000

    Refactor of views.py, all html code reviewed/updated for pep8

--- booking/views.py
+++ booking/views.py
@@ -5,12 +5,32 @@ from .forms import (BookingForm,
                     EditBookingForm,
                     CreateMenuItemForm,
                     DeleteMenuItemForm)
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from .models import Review, Table, Booking, MenuItem, User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.views import generic, View
 from django.contrib import messages
+
+
+def make_booking(request, table_to_book, submited_booking):
+    """
+    Update the booking form with the table and user then update
+    the form and save this will then make the new booking.
+    this will also update the tables number of bookings
+    """
+    submited_booking.table = table_to_book
+
+    if request.user.is_authenticated:
+        submited_booking.booked_by = request.user
+    else:
+        submited_booking.booked_by = user_admin
+
+    submited_booking.save()
+    table_to_book.add_num_of_bookings()
+    table_to_book.save()
+    messages.success(request, 'Thank you for making a booking with us,'
+                     f'see you on {submited_booking.booking_date}')
 
 
 def check_table_size(table, booking):
@@ -120,24 +140,24 @@ class BookingPage(View):
         user_admin = User.objects.get(username='admin')
         booking_form = BookingForm(data=request.POST)
 
-        def make_booking(table_to_book):
-            """
-            Update the booking form with the table and user then update
-            the form and save this will then make the new booking.
-            this will also update the tables number of bookings
-            """
-            submited_booking.table = table_to_book
+        # def make_booking(table_to_book):
+        #     """
+        #     Update the booking form with the table and user then update
+        #     the form and save this will then make the new booking.
+        #     this will also update the tables number of bookings
+        #     """
+        #     submited_booking.table = table_to_book
 
-            if request.user.is_authenticated:
-                submited_booking.booked_by = request.user
-            else:
-                submited_booking.booked_by = user_admin
+        #     if request.user.is_authenticated:
+        #         submited_booking.booked_by = request.user
+        #     else:
+        #         submited_booking.booked_by = user_admin
 
-            submited_booking.save()
-            table_to_book.add_num_of_bookings()
-            table_to_book.save()
-            messages.success(request, 'Thank you for making a booking with us,'
-                             f'see you on {submited_booking.booking_date}')
+        #     submited_booking.save()
+        #     table_to_book.add_num_of_bookings()
+        #     table_to_book.save()
+        #     messages.success(request, 'Thank you for making a booking with us,'
+        #                      f'see you on {submited_booking.booking_date}')
 
         if booking_form.is_valid():
             submited_booking = booking_form.save(commit=False)
@@ -148,8 +168,10 @@ class BookingPage(View):
                 for booking in bookings:
                     if (booking.first_name == submited_booking.first_name
                        and booking.last_name == submited_booking.last_name
-                       and booking.booking_date == submited_booking.booking_date
-                       and booking.booking_time == submited_booking.booking_time):
+                       and booking.booking_date ==
+                       submited_booking.booking_date
+                       and booking.booking_time ==
+                       submited_booking.booking_time):
 
                         has_booked = True
                         break
@@ -161,7 +183,7 @@ class BookingPage(View):
                 free_table = check_available_tables(submited_booking,
                                                     request, False)
                 if free_table:
-                    make_booking(free_table)
+                    make_booking(request, free_table, submited_booking)
 
         elif 'delete-booking' in request.POST:
             print('delete-booking')
@@ -175,6 +197,7 @@ class BookingPage(View):
                 table.remove_num_of_bookings()
                 table.save()
                 booking.delete()
+                messages.warning(request, 'Booking has been deleted')
 
         else:
             booking_form = BookingForm()
@@ -256,44 +279,39 @@ class ManagementPage(LoginRequiredMixin, View):
             if table:
                 print(table)
                 table.delete()
-                messages.success(request, f'{table} deleted')
+                messages.warning(request, f'{table} deleted')
             else:
-                messages.warning(request, 'table does not exist')
+                messages.info(request, 'table does not exist')
                 print('Does not exist')
 
         elif 'delete-booking' in request.POST:
-            print('delete-booking')
             delete_booking = DeleteBookingForm(data=request.POST)
             print(delete_booking)
 
             if delete_booking.is_valid():
-                print("delete-booking Form is valid")
                 booking = get_object_or_404(Booking, id=request.POST['id'])
                 table = booking.table
                 table.remove_num_of_bookings()
                 table.save()
                 booking.delete()
+                messages.warning(request, 'Booking has been deleted')
 
         elif 'create-menu-item' in request.POST:
-            print('create-menu-item called')
             create_menu_item = CreateMenuItemForm(request.POST, request.FILES)
 
             if create_menu_item.is_valid():
-                print('create-menu-item form is valid')
                 create_menu_item.save()
                 messages.success(request, 'New menu item created')
 
         elif 'delete-menu-item' in request.POST:
-            print('delete-menu-item called')
             delete_menu_item = DeleteMenuItemForm(data=request.POST)
             print(request.POST['name'])
             menuItem = get_object_or_404(MenuItem, id=request.POST['name'])
             print(menuItem)
 
             if delete_menu_item.is_valid():
-                print('create-menu-item form is valid')
                 menuItem.delete()
-                messages.success(request, 'Item was deleted')
+                messages.warning(request, 'Item was deleted')
 
         elif 'edit-booking' in request.POST:
             edit_booking = EditBookingForm(data=request.POST)
