b0dcd2555cd61a1d26c03b25f7203a161e8e5b58
Sean Clark <seaniboy2009@yahoo.co.uk>
2023-01-26 16:21:28 +0000

    add booking check and confirmation window with details of the booking

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -17,11 +17,13 @@ document.addEventListener("DOMContentLoaded", () => {
         let deleteMenuButton = document.getElementById("delete-menu-button");
         let deleteTableButton = document.getElementById("delete-table-button");
         let deleteBookingButton = document.getElementById("delete-booking-button");
+        let bookingButton = document.getElementById("booking-button");
 
         // Set all the buttons to not visible
-        deleteTableButton.style.display = "none"
-        deleteMenuButton.style.display = "none"
-        deleteBookingButton.style.display = "none"
+        deleteTableButton.style.display = "none";
+        deleteMenuButton.style.display = "none";
+        deleteBookingButton.style.display = "none";
+        bookingButton.style.display = "none";
 
         if (buttonClicked.value === "delete-table") {
 
@@ -39,6 +41,19 @@ document.addEventListener("DOMContentLoaded", () => {
             deleteBookingButton.style.display = "block"
             modelHeading.innerText = `Confirm booking deletion`;
             modelBody.innerText = `Please confirm you want to delete this booking, once deleted this can not be undone.`;
+        } else if (buttonClicked.value === "submit-booking") {
+
+            bookingButton.style.display = "block";
+            form = bookingButton.form;
+            number_of_guests = form.elements["number_of_guests"].value;
+            booking_date_month = form.elements["booking_date_month"].value;
+            booking_date_day = form.elements["booking_date_day"].value;
+            booking_date_year = form.elements["booking_date_year"].value;
+            booking_time = form.elements["booking_time"].value;
+            modelHeading.innerText = `Confirm booking`;
+            modelBody.innerText = `For ${number_of_guests} People
+                                    On: ${booking_date_month}/${booking_date_day}/${booking_date_year} 
+                                    At: ${booking_time}`;
         }
     }
     
